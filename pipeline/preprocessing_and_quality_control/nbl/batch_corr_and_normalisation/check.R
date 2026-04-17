10a8a4ab33e09fbe9e28ca441ad45893ef6703ee
Elton <eltontobi8@gmail.com>
2026-04-17 17:05:41 +0200

    repository redesign

--- pipeline/preprocessing_and_quality_control/nbl/batch_corr_and_normalisation/check.R
+++
@@ -1,7 +0,0 @@
-
-
-#/usr/bin/env Rscript
-
-path = '/Users/eltonugbogu/Library/CloudStorage/GoogleDrive-eltontobi8@gmail.com/My Drive/dsmz/pipeline/pipeline/results/pan_cancer_joint_benchmark/inputs/joint_expr_matrix.rds'
-
-readRDS(path)
